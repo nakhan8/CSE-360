36af134b943986e62bfacdcc3211c1f9fe7a51eb
nakhan8 <nakhan8@asu.edu>
2019-03-12 22:45:22 -0700

    Assignment 1

---
+++ cse360assign1/OrderedIntList.java
@@ -0,0 +1,89 @@
+package cse360assign1;
+
+/** Ordered list of up to 10 integers
+ * @author Nabeel Asif
+ * @version Feb 3, 2019
+ * @ID 435
+ * Assignment 1
+ */
+
+public class OrderedIntList
+{
+	private int[] data;
+	private int count;
+	
+	/** Create an empty list */
+	OrderedIntList()
+	{
+		data = new int[10];
+		count = 0;
+	}
+	
+	/** Insert unique integer 
+	 *  @param value	integer added to array
+	 */
+	public void insert(int value) 
+	{
+		int insertIndex = getInsertIndex(value);
+		
+		if (insertIndex > 9 || data[insertIndex] == value)
+		{
+			//do nothing if insertIndex is larger than array 
+			// or if value is not unique
+			return;
+		}
+		else if (count < 10)
+		{
+			//shift memory only for what is in the array
+			shift(insertIndex, count);
+						
+			//increment count
+			count = count + 1;
+		}
+		else if(count == 10)
+		{
+			//shift all memory
+			shift(insertIndex, 9);
+		}
+
+		//store data
+		data[insertIndex] = value;
+	}
+
+	private int getInsertIndex(int value) {
+		int insertIndex = 0;
+		for (int index = 0; index < count && value > data[index]; index++)
+		{
+			insertIndex++;
+		}
+		return insertIndex;
+	}
+
+	private void shift(int insertIndex, int count) {
+		for (int index = count; index > insertIndex; index--)
+		{
+			data[index] = data[index - 1];
+		}
+	}
+	
+	/** Displays the list */
+	public void print() 
+	{
+		for (int index = 0; index < count; index++)
+		{
+			System.out.print(data[index]);
+			
+			if(index == count - 1)
+			{
+			}
+			else if ((index + 1) % 5 == 0)
+			{
+				System.out.print("\r\n");
+			}
+			else if (index < count)
+			{
+				System.out.print("\t");
+			}			
+		}
+	}
+}
---
+++ cse360assign1/mainorder.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse360assign1;
+
+import java.util.ArrayList;
+import java.util.*;
+public class mainorder {
+
+	public static void main(String args[]){
+	 {
+                int count = 0;
+		for (int index = 0; index < count; index++)
+		{
+                    boolean[] data = null;
+			System.out.print(data[index]);
+			
+			if(index == count - 1)
+			{
+			}
+			else if ((index + 1) % 5 == 0)
+			{
+				System.out.print("\r\n");
+			}
+			else if (index < count)
+			{
+				System.out.print("\t");
+			}			
+		}
+	}
+      }}
